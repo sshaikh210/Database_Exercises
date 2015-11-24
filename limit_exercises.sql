368bb82b18792dac59b3839a90bc8622af7e3ab0
Sakib Shaikh <sakibshaikh@Sakibs-MacBook-Air.local>
2015-11-24 09:46:47 -0600

    Limit Exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,3 @@
+SELECT DISTINCT last_name FROM employees GROUP BY last_name DESC LIMIT 10;
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%12-25%' ORDER BY birth_date ASC, hire_date DESC LIMIT 5;
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%12-25%' ORDER BY birth_date ASC, hire_date DESC LIMIT 5 OFFSET 45;
