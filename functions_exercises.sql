9eef21670e1122dab441ed2201349201bf142537
Sakib Shaikh <sakibshaikh@Sakibs-MacBook-Air.local>
2015-11-24 10:47:22 -0600

    Functions Exercise

---
+++ functions_exercises.sql
@@ -0,0 +1,13 @@
+SELECT COUNT (gender), gender FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') GROUP BY gender;SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E' ORDER BY emp_no DESC;
+
+SELECT DATEDIFF(NOW(), hire_date), first_name, last_name, hire_date 
+FROM employees 
+WHERE hire_date 
+BETWEEN '1990-01-01' 
+AND '1999-12-31' 
+AND birth_date LIKE '%12-25%' 
+ORDER BY hire_date DESC, birth_date ASC;SELECT * FROM employees WHERE birth_date LIKE '%12-25%';
+
+SELECT COUNT(last_name), first_name, last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name, first_name ORDER BY COUNT(last_name) DESC;
+
+SELECT CONCAT(first_name,' ', last_name) FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E' ORDER BY emp_no DESC;
--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,5 +1,5 @@
-SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
-SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
-SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%12-25%';
+SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY last_name DESC, first_name DESC;
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E' ORDER BY emp_no DESC;
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%12-25%' ORDER BY hire_date DESC, birth_date ASC;
 SELECT * FROM employees WHERE birth_date LIKE '%12-25%';
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
